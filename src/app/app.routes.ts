1b985e1dadeb592934a748fecb292b5b93b2ae33
Audric Serador <audricciel@gmail.com>
2024-02-02 13:10:37 -0600

    Fix Angular/Express setup, initialized Firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LandingComponent } from './landing/landing.component';
+import { RecordingComponent } from './recording/recording.component';
+import { ReviewComponent } from './review/review.component';
+import { UploadComponent } from './upload/upload.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
+  { path: 'record', component: RecordingComponent },
+  { path: 'review', component: ReviewComponent },
+  { path: 'upload', component: UploadComponent }
+];
---
+++ src/app/recording/recording.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recording',
+  standalone: true,
+  imports: [],
+  templateUrl: './recording.component.html',
+  styleUrl: './recording.component.css'
+})
+export class RecordingComponent {
+
+}
